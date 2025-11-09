acf977ba5586bcbc135695e7b88ccdb091111f5c
Marco <mespinozapoma@gmail.com>
2025-11-09 10:17:30 -0300

    Deploy EcoMarket to GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/EcoMarket/', 
   plugins: [react()],
-  base: '/ecomarket/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://690e6475bd0fefc30a045746.mockapi.io',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
