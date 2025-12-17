f32dd9be668b8fa95c2883030922364295337a24
Marco <mespinozapoma@gmail.com>
2025-12-16 21:05:57 -0300

    vite config fix for build issue

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/',  // Para Vercel
   plugins: [react({
-    jsxRuntime: 'automatic' 
+    jsxRuntime: 'automatic'
   })],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false,  // Opcional: acelera el build
+    emptyOutDir: true  // Limpia la carpeta dist antes de build
   }
 })
